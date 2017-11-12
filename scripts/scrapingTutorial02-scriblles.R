03afd2b5598c2b2826f85ef2033a20f6ad0797f4
ogorodriguez <ogorodriguez@gmail.com>
2017-11-12 11:23:13 +0100

    Added a script to understand how to extract values from a list item and make it into a table.

--- scripts/scrapingTutorial02-scriblles.R
+++ scripts/scrapingTutorial02-scriblles.R
@@ -11,3 +11,27 @@ data_frame(text = readLines("https://cran.r-project.org/web/views/WebTechnologie
   print(n = Inf)
 
 # It appears 80 times
+
+===========================
+  
+# Study the following code to extract information from a list
+  
+# create empty data frame to fill  
+layoff_df <- data.frame(NULL)
+# extract data of interest from each nested year-month list  
+
+for(i in seq_along(layoffs$Results$series[[1]]$data)) {
+  df <- data.frame(layoffs$Results$series[[1]]$data[i][[1]][1:4])
+  layoff_df <- rbind(layoff_df, df)
+}
+
+layoff_df %>% mutate(valueNum = as.integer(value)) %>% arrange(desc(valueNum))
+
+glimpse(layoff_df)
+
+# Disecting the previous code in item 23
+for(i in seq_along(layoffs$Results$series[[1]]$data)) {
+  df <- data.frame(layoffs$Results$series[[1]]$data[i][[1]][1:4])
+}
+seq_along(layoffs$Results$series[[1]]$data)
+df
